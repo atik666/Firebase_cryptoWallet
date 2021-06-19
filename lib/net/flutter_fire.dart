74edc2df82c5e663fcc04a467d95060c40ad4004
Atik Faysal <faisal.atik@gmail.com>
2021-06-20 00:44:34 +0800

    First 6/20

---
+++ lib/net/flutter_fire.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+Future<bool> signIn(String email, String password) async {
+  try {
+    await FirebaseAuth.instance.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+    return true;
+  } on FirebaseAuthException catch (e) {
+    print('Failed with error code: ${e.code}');
+    print(e.message);
+    return false;
+  }
+}
+
+Future<bool> register(String email, String password) async {
+  try {
+    await FirebaseAuth.instance.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+    return true;
+  } on FirebaseAuthException catch (e) {
+    print('Failed with error code: ${e.code}');
+    print(e.message);
+    return false;
+  }
+}
---
+++ lib/ui/auth.dart
@@ -0,0 +1,87 @@
+import 'package:crypto_wallet/net/flutter_fire.dart';
+import 'package:flutter/material.dart';
+
+class Auth extends StatefulWidget {
+  const Auth({Key? key}) : super(key: key);
+
+  @override
+  _AuthState createState() => _AuthState();
+}
+
+class _AuthState extends State<Auth> {
+  TextEditingController _emailFiled = TextEditingController();
+  TextEditingController _password = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+          color: Colors.blueAccent,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            TextFormField(
+              controller: _emailFiled,
+              decoration: InputDecoration(
+                hintText: "Input your email",
+                labelText: "Email",
+                labelStyle: TextStyle(color: Colors.white),
+                hintStyle: TextStyle(color: Colors.white),
+              ),
+            ),
+            TextFormField(
+              controller: _password,
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: "Input your password",
+                labelText: "Password",
+                labelStyle: TextStyle(color: Colors.white),
+                hintStyle: TextStyle(color: Colors.white),
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width / 1.4,
+              height: 45,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color: Colors.white,
+              ),
+              child: MaterialButton(
+                onPressed: () async {
+                  bool shouldNavigate =
+                      await register(_emailFiled.text, _password.text);
+                  if (shouldNavigate) {
+                    //Navigate
+                  }
+                },
+                child: Text("Register"),
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width / 1.4,
+              height: 45,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color: Colors.white,
+              ),
+              child: MaterialButton(
+                onPressed: () async {
+                  bool shouldNavigate =
+                      await signIn(_emailFiled.text, _password.text);
+                  if (shouldNavigate) {
+                    //Navigate
+                  }
+                },
+                child: Text("Login"),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
